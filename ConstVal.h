b802adcad0155fc0f350b9d78ebaa4e63180df2a
AvitalTse <112376760+AvitalTse@users.noreply.github.com>
2022-08-29 13:08:43 +0300

    Update ConstVal.h

---
+++ ConstVal.h
@@ -0,0 +1,32 @@
+
+/*************************** A file containing all the settings of the constants ****************************/
+
+#define MAX_CHARS 30
+#define MAX_DATA_DIR 3
+#define EX_EN 2
+#define MAX_CODE_INS 16
+#define ARE 3
+#define MAX_REGIS 8
+#define ADD_METHOD 4
+#define MAX_ROW_LENGTH 80
+#define DATA_BITS 10
+#define CODE_BITS 8
+#define NO_ENCODE 'N'
+#define ABS_ENCODE 'A'
+#define REL_ENCODE 'R'
+#define EX_ENCODE 'E'
+#define WORD_LEN_STR 11
+#define WORD_LEN_BITS 10
+#define MACRO "macro"
+#define ENDMACRO "endmacro"
+#define CPU_BASE 32
+#define CPU_SIGN 3
+#define NEG '-'
+#define POS '+'
+#define MEM_INT 100
+#define CPU_BITS 5
+#define CHECK_LEN 100
+#define MIN_LINES 156
+#define MAX_MEM 256
+#define BUFFER_SIZE 4000
+
