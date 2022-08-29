19ede593f70d6220ec2aa4ba58f5ebd9f5389895
AvitalTse <112376760+AvitalTse@users.noreply.github.com>
2022-08-29 13:08:07 +0300

    Update ConstTab.h

---
+++ ConstTab.h
@@ -0,0 +1,14 @@
+#include "ConstVal.h" 
+
+/******************************A file for defining fixed reference tables******************************/
+
+extern const char data[MAX_DATA_DIR][MAX_CHARS];
+extern const char ex_en[EX_EN][MAX_CHARS];
+extern const char code[MAX_CODE_INS][MAX_CHARS];
+extern const char code_bin[MAX_CODE_INS][MAX_CHARS];
+extern const char A_R_E[ARE][MAX_CHARS];
+extern const char regi[MAX_REGIS][MAX_CHARS];
+extern const char add_method_bin[ADD_METHOD][MAX_CHARS];
+extern const char regi_bin[MAX_REGIS][MAX_CHARS];
+extern const char CPU_LANG[CPU_BASE];
+
